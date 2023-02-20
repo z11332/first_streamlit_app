23f06aa0d8db1a431cc53b76981bf77d21e606b6
z11332 <zhang_jet@yahoo.ca>
2023-02-20 09:41:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My parent new healthy dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 and Omeal egg')
@@ -6,3 +7,6 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocato Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
