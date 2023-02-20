aad1d8e31ae8245e480f289a28ad2d33d9b6ebe3
z11332 <zhang_jet@yahoo.ca>
2023-02-20 09:23:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My parent new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Omeal egg')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 and Omeal egg')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlig.text('🥑🍞Avocato Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
